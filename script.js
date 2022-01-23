db828de60a2191c56ca2c042a0f00e8cb45f9b6a
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-23 10:58:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -113,6 +113,7 @@ var month_name = function(dt){
 
 
 
+
 var compare_dates = function(date1,date2){
     if (date1>date2) return ("Date1 > Date2");
   else if (date1<date2) return ("Date2 > Date1");
