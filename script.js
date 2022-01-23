ed4b3fc51c7e56f6ece98cbe0d397b96f0b7e37a
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-23 10:56:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -89,6 +89,7 @@ var getDaysInMonth = function(month,year) {
 
 
 
+
 var month_name = function(dt){
     mlist = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
       return mlist[dt.getMonth()];
